a72be77ccf35fe2e96b6989694595b6c3fb4630d
Olivier Xu <olivier.xu@gmail.com>
2014-01-20 15:13:06 +0100

    first attempt at 00 ex6

--- 06-Instance-vs-Class/restaurant.rb
+++ 06-Instance-vs-Class/restaurant.rb
@@ -1,3 +1,40 @@
 class Restaurant
-  # Your turn !
+
+	attr_reader :name, :rating, :nb_ratings
+	
+	@total_nb_ratings = 0
+	
+	class << self
+		attr_accessor :total_nb_ratings
+	end
+
+	def initialize(name)
+		@name = name
+		@rating = 0
+		@nb_ratings = 0
+	end
+
+
+	def rate(rating)
+		@rating = (@rating * @nb_ratings + rating ) / (nb_ratings + 1)
+		@nb_ratings += 1
+		Restaurant.total_nb_ratings += 1
+		@rating
+	end
+		
 end
+
+
+resto = Restaurant.new("Coco")
+
+puts resto.rate(5) # => 5 ?
+puts "Nb of ratings: #{resto.nb_ratings}"
+
+puts resto.rate(7) # => 6 ?
+puts "Nb of ratings: #{resto.nb_ratings}"
+
+puts resto.rate(9) # => 7 ?
+puts "Nb of ratings: #{resto.nb_ratings}"
+
+10.times { Restaurant.new("test").rate(10) }
+puts "Total nb of ratings: #{Restaurant.total_nb_ratings} " # => 13 ?
